3c5a1698289edf5f40ed0ba174477ee8c20bc74e
Kostas Chaidos <konstantinos.chaidos@orfium.com>
2023-10-22 11:16:57 +0300

    Use celery pytest and testcontainers with localstack

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,80 @@
+from unittest.mock import MagicMock
 import asyncpg
+from celery import Celery
 import httpx
 import pytest
 from sqlalchemy import make_url
-from fastapi_async_celery.config.depends import get_db_url
+from fastapi_async_celery.config.depends import get_celery_app, get_db_url
+from fastapi_async_celery.create_celery_app import create_celery_app
 from fastapi_async_celery.main import app
 from fastapi_async_celery.config.db_model_base import Base
+from testcontainers.localstack import LocalStackContainer
 from testcontainers.postgres import PostgresContainer
 from sqlalchemy.ext.asyncio import create_async_engine
+from celery.signals import task_postrun
+import time
+
+pytest_plugins = ("celery.contrib.pytest",)
 
 
 @pytest.fixture(scope="session")
 def anyio_backend() -> str:
     return "asyncio"
+
+
+@pytest.fixture(scope="session")
+def localstack_sqs():
+    with LocalStackContainer(image="localstack/localstack") as localstack:
+        localstack.with_services("sqs")
+        host = localstack.get_container_host_ip()
+        port = localstack.get_exposed_port(LocalStackContainer.EDGE_PORT)
+        yield f"{host}:{port}"
+
+
+@pytest.fixture(scope="session")
+def celery_config(localstack_sqs, executed_celery_tasks):
+    @task_postrun.connect(weak=False)
+    def task_postrun_handler(task_id, task, retval, state, *args, **kwargs):
+        executed_celery_tasks.append(
+            (task.name, kwargs["args"], kwargs["kwargs"], retval, state)
+        )
+
+    import os
+
+    if "CELERY_BROKER_URL" in os.environ:
+        os.environ.pop("CELERY_BROKER_URL")
+    return {
+        "broker_url": f"sqs://fake:fake@{localstack_sqs}/0",  # pragma: allowlist secret
+        "broker_transport_options": {"wait_time_seconds": 2},
+    }
+
+
+@pytest.fixture(scope="session")
+def executed_celery_tasks():
+    return []
+
+
+@pytest.fixture(scope="function")
+def test_executed_celery_tasks(executed_celery_tasks):
+    executed_celery_tasks.clear()
+    return executed_celery_tasks
+
+
+@pytest.fixture(scope="function")
+def wait_for_task(test_executed_celery_tasks):
+    def _wait_for_task(task_name, args, kwargs, retval, state, max_wait=10):
+        start_time = time.monotonic()
+        expected_execution = (task_name, args, kwargs, retval, state)
+        while True:
+            if expected_execution in test_executed_celery_tasks:
+                return
+            if time.monotonic() - start_time > max_wait:
+                assert (
+                    False
+                ), f"Expected {expected_execution} found {test_executed_celery_tasks}"
+            time.sleep(0.2)
+
+    return _wait_for_task
 
 
 @pytest.fixture(scope="session")
@@ -64,20 +127,57 @@ async def reset_db(db_engine):
             await conn.execute(table.delete())
 
 
+@pytest.fixture(scope="session")
+def fac_celery_app(db_dsn, celery_session_app, celery_session_worker):
+    drs_celery_app = create_celery_app(
+        celery_app=celery_session_app,
+        db_url=db_dsn,
+    )
+    celery_session_worker.reload()
+    return drs_celery_app
+
+
 @pytest.fixture(scope="function")
-async def app_with_test_db(db_dsn, reset_db):
+async def app_with_test_db_and_celery(db_dsn, fac_celery_app, reset_db):
     def override_get_db_url():
         return db_dsn
 
+    def override_get_celery_app():
+        return fac_celery_app
+
     app.dependency_overrides[get_db_url] = override_get_db_url
+    app.dependency_overrides[get_celery_app] = override_get_celery_app
+    yield app
+
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture(scope="function")
+async def app_with_test_db_no_celery(db_dsn, reset_db):
+    def override_get_db_url():
+        return db_dsn
+
+    def override_get_celery_app():
+        return MagicMock(spec=Celery)
+
+    app.dependency_overrides[get_db_url] = override_get_db_url
+    app.dependency_overrides[get_celery_app] = override_get_celery_app
     yield app
 
     app.dependency_overrides.clear()
 
 
 @pytest.fixture
-async def async_client(app_with_test_db):
+async def async_client(app_with_test_db_no_celery):
     async with httpx.AsyncClient(
-        app=app_with_test_db, base_url="http://test"
+        app=app_with_test_db_no_celery, base_url="http://test"
+    ) as client:
+        yield client
+
+
+@pytest.fixture
+async def async_client_with_celery(app_with_test_db_and_celery):
+    async with httpx.AsyncClient(
+        app=app_with_test_db_and_celery, base_url="http://test"
     ) as client:
         yield client
--- tests/fastapi_async_celery/s3_char_count/test_s3_char_count_api.py
+++ tests/fastapi_async_celery/s3_char_count/test_s3_char_count_api.py
@@ -1,6 +1,7 @@
 from unittest import mock
 from httpx import AsyncClient
 import pytest
+from uuid import UUID
 
 from fastapi_async_celery.s3_char_count.schema import (
     Batch,
@@ -12,9 +13,11 @@ from fastapi_async_celery.s3_char_count.schema import (
 
 class TestS3CharCountApi:
     @pytest.mark.anyio
-    async def test_create_batch(self, async_client: AsyncClient):
+    async def test_create_batch(
+        self, async_client_with_celery: AsyncClient, wait_for_task
+    ):
         s3_path = "s3://bucket/key"
-        response = await async_client.post(
+        response = await async_client_with_celery.post(
             url="v1/s3_char_count/batch",
             json=BatchIn(s3_path=s3_path).model_dump(),
         )
@@ -24,6 +27,17 @@ class TestS3CharCountApi:
             "status": {"status": BatchStatus.REGISTERED.value},
             "id": mock.ANY,
         }
+        batch_id = UUID(response.json()["id"])
+        wait_for_task(
+            task_name="handle_batch",
+            args=[],
+            kwargs={
+                "id": batch_id,
+            },
+            retval=None,
+            state="SUCCESS",
+            max_wait=3,
+        )
 
     @pytest.mark.anyio
     async def test_create_and_retrieve_batch(self, async_client: AsyncClient):
